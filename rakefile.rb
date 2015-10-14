cb72de1651323e2d93ac73634d49900ff7e0773b
Bart Slinger <bartslinger@gmail.com>
2015-10-14 20:05:55 +0200

    ceedling removed, too difficult to configure

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,32 @@
-PROJECT_CEEDLING_ROOT = "vendor/ceedling"
-load "#{PROJECT_CEEDLING_ROOT}/lib/ceedling.rb"
+HERE = File.expand_path(File.dirname(__FILE__)) + '/'
 
-Ceedling.load_project
+require 'rake'
+require 'rake/clean'
+require 'rake/testtask'
+require './rakefile_helper'
 
-task :default => %w[ test:all release ]
+include RakefileHelpers
+
+# Load default configuration, for now
+DEFAULT_CONFIG_FILE = 'Helisa.yml'
+configure_toolchain(DEFAULT_CONFIG_FILE)
+
+task :unit do
+  run_tests(get_unit_test_files)
+end
+
+desc "Generate test summary"
+task :summary do
+  report_summary
+end
+
+desc "Build and test Unity"
+task :all => [:clean, :unit, :summary]
+task :default => [:clobber, :all]
+task :ci => [:default]
+task :cruise => [:default]
+
+desc "Load configuration"
+task :config, :config_file do |t, args|
+  configure_toolchain(args[:config_file])
+end
